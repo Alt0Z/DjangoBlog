5f69982790349c85755a44c43f2f54120eb042e8
RomanShakhanov <altair0908@gmail.com>
2017-07-15 12:56:49 +0300

    about page and small fixes for posting display

--- myblog/views.py
+++ myblog/views.py
@@ -9,3 +9,11 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'myblog/post_detail.html', {'post': post})
+
+def about_page(request):
+    about = get_object_or_404
+    return render(request, 'myblog/about.html', {'about': about})
+
+def contacts(request):
+    contact = get_object_or_404
+    return render(request, 'myblog/contacts.html', {'contact': contact})
