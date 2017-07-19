29a605d5fdd9d537a7ecffcbc7250083b2954b96
RomanShakhanov <altair0908@gmail.com>
2017-07-19 20:15:22 +0300

    Added possibility to create posts in real time

--- myblog/urls.py
+++ myblog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^about/$', views.about_page, name='about_page'),
     url(r'^contacts/$', views.contacts, name='contacts'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
