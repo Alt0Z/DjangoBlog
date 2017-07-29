23e6f0e8b5ee057ea6527b529b7b23656cbbbe33
RomanShakhanov <altair0908@gmail.com>
2017-07-29 17:42:39 +0300

    Fixed post approved method

--- myblog/forms.py
+++ myblog/forms.py
@@ -8,7 +8,7 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'text',)
 
 class CommentForm(forms.ModelForm):
-    
+
     class Meta:
         model = Comment
-        fields = ('author', 'text')
+        fields = ('author', 'text',)
